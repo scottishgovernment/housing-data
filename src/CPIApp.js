4c06b7f6e34b15523f83ca11d1666856abe2b896
David Sinclair <david.sinclair@gov.scot>
2017-06-08 16:28:46 +0100

    MGS-3128 fixed health endpoint

--- src/CPIApp.js
+++ src/CPIApp.js
@@ -7,48 +7,41 @@ const dateUtils = require('./dateUtils');
 
 class CPIApp {
 
-     constructor(source) {
-         this.source = source;
-     }
+    constructor(source) {
+        this.source = source;
+    }
 
-     register(expressApp) {
-         expressApp.get('/cpi', (req, res) => {
-             this.source.get((error, cpi) => {
-                 if (error) {
-                     res.status(500).send(error);
-                 } else {
-                     res.json(cpi);
-                 }
-             });
-         });
+    register(expressApp) {
+        expressApp.get('/cpi', (req, res) => {
+            this.source.get((error, cpi) => {
+                if (error) {
+                    res.status(500).send(error);
+                } else {
+                    res.json(cpi);
+                }
+            });
+        });
 
-         expressApp.get('/healthcheck', (req, res) => {
-             this.source.get((error, cpi) => {
-                 if (error) {
-                     res.status(500).send({ msg: error });
-                 } else {
-                    var problems = [];
+        expressApp.get('/health', (req, res) => {
+            this.source.get((error, cpi) => {
+                if (error) {
+                    res.status(500).send({ msg: error });
+                    return;
+                }
 
-                    // now >= cpiReleaseDate
-                    const releaseDateTodayOrBefore = dateUtils.compareWithNow(cpi.releaseDate) >= 0;
-                    if (!releaseDateTodayOrBefore) {
-                        problems.push('Release date should be today or before: ' + cpi.releaseDate);
-                    }
-
-                    // nextRelease < now
-                    const nextReleaseInFuture = dateUtils.compareWithNow(cpi.nextRelease) < 0;
-                    if (!nextReleaseInFuture) {
-                        problems.push('Next release should be in the future: ' + cpi.nextRelease);
-                    }
-
-                    res.json({
-                        message: problems.length === 0 ? 'OK': 'Problems',
-                        details: problems
-                    });
-                 }
-             });
-         });
-     }
+                const releaseDateTodayOrBefore = dateUtils.compareWithNow(cpi.releaseDate) >= 0;
+                const nextReleaseInFuture = dateUtils.compareWithNow(cpi.nextRelease) < 0;
+                const ok = releaseDateTodayOrBefore && nextReleaseInFuture;
+                const status = ok ? 200 : 503;
+                const message = 'releaseDate: ' + cpi.releaseDate
+                                + ', nextRelease: ' + cpi.nextRelease;
+                res.status(status).send({
+                    ok: ok,
+                    message: message
+                });
+            });
+        });
+    }
 }
 
 module.exports = CPIApp;
