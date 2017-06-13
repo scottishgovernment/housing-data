0a84efe7bc2b7bc7dcb2887691db43ba7aef0de0
David Sinclair <david.sinclair@gov.scot>
2017-06-13 11:13:29 +0100

    MGS-3128 fixed bug in next release date parsing

--- src/CPIParser.js
+++ src/CPIParser.js
@@ -98,24 +98,16 @@ function monthIndex(monthStr) {
 }
 
 function convertReleaseDate(input) {
-      // format used in the csv file for release date: 16-05-2017
-      const format = 'dd/mm/yyyy';
-      var parts = input.match(/(\d+)/g),  i = 0, fmt = {};
-      // extract date-part indexes from the format
-      format.replace(/(yyyy|dd|mm)/g, function(part) { fmt[part] = i++; });
-      return [parts[fmt['yyyy']], parts[fmt['mm']], parts[fmt['dd']]].join('-');
+    // format used in the csv file for release date: 16-05-2017
+    const format = 'dd/mm/yyyy';
+    var parts = input.match(/(\d+)/g),  i = 0, fmt = {};
+    // extract date-part indexes from the format
+    format.replace(/(yyyy|dd|mm)/g, function(part) { fmt[part] = i++; });
+    return [parts[fmt['yyyy']], parts[fmt['mm']], parts[fmt['dd']]].join('-');
 }
 
 function convertNextReleaseDate(input) {
-      // format used in the csv file for next release date: 13 Jun 2017
-      const parts = input.split(' ');
-      const monthIndexRes = monthIndex(parts[1].toUpperCase());
-      const newParts = [
-        parts[2],
-        dateUtils.pad(2, monthIndexRes),
-        dateUtils.pad(2, parts[0])];
-      return newParts.join('-');
+    return dateUtils.dateString(new Date(input));
 }
-
 
 module.exports = CPIParser;
