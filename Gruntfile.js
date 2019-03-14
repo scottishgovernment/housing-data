7f23f1a7173def867c5585dbfbff866266cddc59
Martin Ellis <martin.ellis@gov.scot>
2019-03-14 15:12:13 +0000

    MGS-4979 Make JSON schema validation work on CouchDB 2.x
    The jsen library previously used for validation documents
    against a JSON schema generated source for a validator,
    then used `eval` to execute it. This doesn't work on
    CouchDB 2.x, where CSP rules prevent this operation.
    
    This change switches to the ajv library for JSON validation.
    The ajv-pack library is used to generate the source for the
    schema validator at build time, so does not rely on `eval`.
    
    The generated validator requires some runtime classes from
    ajv, and they are copied into the couch app directory by
    the build.
    
    This change also changes the program used to create the
    CouchDB design document. This used to be couchdb-push,
    which was installed as an NPM package. This has been
    changed to couchapp, which can be installed using packages
    included with the operating system.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,30 @@
 var fs = require('fs');
 var path = require('path');
 
+// Generate schema validation code from the schema
+function generateSchemaValidator() {
+  var Ajv = require('ajv');
+  var pack = require('ajv-pack');
+  var ajv = new Ajv({
+    allErrors: true,
+    sourceCode: true
+  });
+
+  var dir = path.join(__dirname, 'couch');
+  var json = fs.readFileSync(path.join(dir, 'cpi.json'));
+  var schema = JSON.parse(json);
+  var validate = ajv.compile(schema);
+  var moduleCode = pack(ajv, validate);
+  fs.writeFileSync(path.join(dir, 'validate.js'), moduleCode.toString());
+
+  dir = path.join(__dirname, 'couch-rpz');
+  json = fs.readFileSync(path.join(dir, 'rpz.json'));
+  schema = JSON.parse(json);
+  validate = ajv.compile(schema);
+  moduleCode = pack(ajv, validate);
+  fs.writeFileSync(path.join(dir, 'validate.js'), moduleCode.toString());
+}
+
 module.exports = function(grunt) {
 
     var pkg = grunt.file.readJSON('package.json');
@@ -11,6 +35,10 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-jasmine-node');
     grunt.loadNpmTasks('grunt-istanbul');
     grunt.loadNpmTasks('grunt-sonar-runner');
+    grunt.task.registerTask(
+        'validator',
+        'Generates a validator for the CouchDB schema',
+        generateSchemaValidator);
 
     var tokenFile = path.join(process.env.HOME, '.sonar/token');
     var token;
@@ -50,6 +78,48 @@ module.exports = function(grunt) {
     grunt.initConfig({
 
         copy: {
+            couchdb: {
+              files: [
+                {
+                  expand: true,
+                  cwd: "node_modules",
+                  src: [
+                    "ajv/lib/compile/formats.js",
+                    "ajv/lib/compile/util.js",
+                    "ajv/lib/compile/ucs2length.js"
+                  ],
+                  dest: "couch"
+                },
+                {
+                  expand: true,
+                  cwd: "node_modules",
+                  src: "fast-deep-equal/index.js",
+                  dest: "couch",
+                  rename: function(dest, src) {
+                    return dest + "/fast-deep-equal.js";
+                  }
+                },
+                {
+                  expand: true,
+                  cwd: "node_modules",
+                  src: [
+                    "ajv/lib/compile/formats.js",
+                    "ajv/lib/compile/util.js",
+                    "ajv/lib/compile/ucs2length.js"
+                  ],
+                  dest: "couch-rpz"
+                },
+                {
+                  expand: true,
+                  cwd: "node_modules",
+                  src: "fast-deep-equal/index.js",
+                  dest: "couch-rpz",
+                  rename: function(dest, src) {
+                    return dest + "/fast-deep-equal.js";
+                  }
+                }
+              ]
+            },
             test: {
                 files: [
                     {
--- couch/validate_doc_update.js
+++ couch/validate_doc_update.js
@@ -1,14 +1,13 @@
 function (doc, old, user, security) {
 
-    var jsen = require('lib/jsen');
-    var schema = require('lib/cpi');
+    var validateCPI = require('validate');
 
     if (doc.type === 'cpi') {
-        var validate = jsen(schema);
-        var valid = validate(doc);
+        var valid = validateCPI(doc);
 
         if (!valid) {
             throw { forbidden: JSON.stringify(validate.errors, null, 2) };
         }
     }
+
 }
