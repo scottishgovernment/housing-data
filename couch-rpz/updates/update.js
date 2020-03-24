52e5ba5455e0820463f7e3f87aeaa6b418a3699f
David Sinclair <david.sinclair@gov.scot>
2020-03-24 17:33:45 +0000

    Fix broken update function

--- couch-rpz/updates/update.js
+++ couch-rpz/updates/update.js
@@ -1,11 +1,14 @@
 function (doc, req) {
-    // copy existing values to the history
-    const clone = JSON.parse(JSON.stringify(doc));
-    delete clone.history;
-    delete clone._revisions;
-    delete clone._id;
-
-    doc.history.push(clone);
+    // if this document already exists then copy existing values to the history
+    if (doc._id) {
+      const clone = JSON.parse(JSON.stringify(doc));
+      delete clone.history;
+      delete clone._revisions;
+      delete clone._id;
+      doc.history.push(clone);
+    } else {
+      doc.history = [];
+    }
 
     // copy values from request body
     const body = JSON.parse(req.body);
