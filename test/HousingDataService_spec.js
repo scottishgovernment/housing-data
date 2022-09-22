1604890f64a0dbaf2a19163a14ae66d99f71252c
Martin Ellis <martin.ellis@gov.scot>
2022-09-22 14:17:33 +0100

    Add test for where no targets are enabled

--- test/HousingDataService_spec.js
+++ test/HousingDataService_spec.js
@@ -5,14 +5,21 @@ var HousingDataService = require('../src/HousingDataService.js');
 describe('HousingDataService', function() {
 
     it('uses S3 target if enabled', function (done) {
-        const cfg = config(false, true)
+        const cfg = config(true)
         const targets = HousingDataService.targets(cfg);
         expect(targets.length).toBe(1);
         expect(targets[0].constructor.name).toBe('S3CPIStore');
         done();
     });
 
-    function config(esEnabled, s3Enabled) {
+    it('uses no targets if none enabled', function (done) {
+        const cfg = config(false)
+        const targets = HousingDataService.targets(cfg);
+        expect(targets.length).toBe(0);
+        done();
+    });
+
+    function config(s3Enabled) {
         return {
             s3: {
                 enabled: s3Enabled,
